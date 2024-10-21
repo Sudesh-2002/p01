23f85be72f72f04b9ec13a28c23f1d5cd204b64b
Sudesh Hansika <sudeshhansika@gmail.com>
2024-10-21 20:21:19 +0530

    change name

--- src/Youtube.jsx
+++ src/Youtube.jsx
@@ -4,8 +4,7 @@ function Youtube(){
     <div className='div1'>
       <video className='vid1' src={video1} controls></video>
       <h2>Ruu chaya song</h2>
-      <p>this is our newly released song. 
-        please follow us on twitter and subscribe our toutube channes</p>
+      <h1>hello</h1>
     </div>
   );
 }
